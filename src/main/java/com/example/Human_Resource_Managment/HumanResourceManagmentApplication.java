4e1697be986e1089289450f2b2c338bf673a7294
Nakulpoonia <poonianakul@gmail.com>
2026-05-13 18:42:57 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/Human_Resource_Managment/HumanResourceManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Human_Resource_Managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Human_Resource_Managment/HumanResourceManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Human_Resource_Managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
