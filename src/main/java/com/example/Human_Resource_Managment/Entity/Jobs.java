a0de257ac13b25c1d5e387a6a899f3fa2aab71e9
samarpitSharma88 <samarpit_sharma@icloud.com>
2026-05-14 10:00:57 +0530

    updated Jobs entity

--- src/main/java/com/example/Human_Resource_Managment/Entity/Jobs.java
+++ src/main/java/com/example/Human_Resource_Managment/Entity/Jobs.java
@@ -1,7 +1,12 @@
 package com.example.Human_Resource_Managment.Entity;
 
-import jakarta.persistence.Id;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Jobs {
     @Id
     long job_id;
