41bc0c0220b04abdb61605e28be5a110cc9bf89e
Nakulpoonia <poonianakul@gmail.com>
2026-05-14 09:57:56 +0530

    Added lombok

--- src/main/java/com/example/Human_Resource_Managment/Entity/Departments.java
+++ src/main/java/com/example/Human_Resource_Managment/Entity/Departments.java
@@ -1,7 +1,11 @@
 package com.example.Human_Resource_Managment.Entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
 public class Departments {
     @Id
     long department_Id;
